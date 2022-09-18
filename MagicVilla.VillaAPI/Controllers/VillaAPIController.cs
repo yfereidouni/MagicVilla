7d6a58b57b1559ea87401cabba701c6b434a8982
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-09-18 15:28:36 +0430

    Using DTO for express model to the endpoint

---
+++ MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla.VillaAPI.Models;
+using MagicVilla.VillaAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>()
+            {
+                new VillaDTO {Id=1, Name = "Pool View" ,CreatedDate=DateTime.Now },
+                new VillaDTO {Id=2, Name = "Beach View",CreatedDate=DateTime.Now }
+            };
+        }
+
+    }
+}
---
+++ MagicVilla.VillaAPI/Models/Villa.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla.VillaAPI.Models;
+
+public class Villa
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+}
