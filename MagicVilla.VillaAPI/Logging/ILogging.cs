ec894543c50a771335c29ee5c7d898b0130821e8
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-19 22:57:02 +0430

    Adding Custom Logging and Log with Serilog

---
+++ MagicVilla.VillaAPI/Logging/ILogging.cs
@@ -0,0 +1,6 @@
+﻿namespace MagicVilla.VillaAPI.Logging;
+
+public interface ILogging
+{
+    public void Log(string message, string type);
+}
--- MagicVilla.VillaAPI/Program.cs
+++ MagicVilla.VillaAPI/Program.cs
@@ -1,9 +1,23 @@
+using MagicVilla.VillaAPI.Logging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Serilog Config --------------------------------------------------------------------------
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.File("Logs/VillaLogs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+builder.Host.UseSerilog();
+//------------------------------------------------------------------------------------------
+
+// Cutstom Logger configuration -------------------------------------------------------------
+builder.Services.AddSingleton<ILogging, Logging>();
+//------------------------------------------------------------------------------------------
+
 // Headers configuration for accepting "Application/XML" content ----------------------------
-builder.Services.AddControllers(options => 
+builder.Services.AddControllers(options =>
 {
     //options.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
