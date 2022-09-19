456f1185fc818dd481bf5afff4b3a06ce89f8842
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-09-19 12:37:54 +0430

    Headers configuration for accepting "Application/XML" content

--- MagicVilla.VillaAPI/Program.cs
+++ MagicVilla.VillaAPI/Program.cs
@@ -2,7 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+// Headers configuration for accepting "Application/XML" content ----------------------------
+builder.Services.AddControllers(options => 
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+//-------------------------------------------------------------------------------------------
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
