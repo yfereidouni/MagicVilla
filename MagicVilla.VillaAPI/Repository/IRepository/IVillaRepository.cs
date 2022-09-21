78fa4b314a6d9b7f728a1ffdb5efb83c2fc12fde
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-09-21 16:15:13 +0430

    done

---
+++ MagicVilla.VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla.VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla.VillaAPI.Repository.IRepository;
+
+public interface IVillaRepository
+{
+    //GetAll : this method can get any LINQ Expression as a filter
+    Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+    Task<List<Villa>> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+    Task Create(Villa entity);
+    Task Remove(Villa entity);
+    Task Save();
+}
---
+++ MagicVilla.VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,42 @@
+﻿using MagicVilla.VillaAPI.Data;
+using MagicVilla.VillaAPI.Models;
+using MagicVilla.VillaAPI.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MagicVilla.VillaAPI.Repository;
+
+public class VillaRepository : IVillaRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public VillaRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Create(Villa entity)
+    {
+        await _dbContext.AddAsync(entity);
+        await Save();
+    }
+
+    public Task<List<Villa>> Get(Expression<Func<Villa>> filter = null, bool tracked = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Remove(Villa entity)
+    {
+        _dbContext.Remove(entity);
+        await Save();
+    }
+
+    public async Task Save()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
