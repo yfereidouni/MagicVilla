7e6bb1f3a63fe85fe2acc87d9be733af3125d8e5
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-22 20:55:11 +0330

    adding ForeignKey to VillaNumber

--- MagicVilla.VillaAPI/Models/DTOs/VillaNumberUpdateDTO.cs
+++ MagicVilla.VillaAPI/Models/DTOs/VillaNumberUpdateDTO.cs
@@ -6,5 +6,7 @@ public class VillaNumberUpdateDTO
 {
     [Required]
     public int VillaNo { get; set; }
+    [Required]
+    public int VillaId { get; set; }
     public string SpecialDetails { get; set; }
 }
