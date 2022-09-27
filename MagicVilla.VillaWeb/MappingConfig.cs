8ec95263ffdce4aecc0f15cd5101493653adba6c
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-27 16:51:20 +0330

    add bootstrap-icons add villaController add baseRepository add baseService

---
+++ MagicVilla.VillaWeb/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla.VillaWeb.Models.DTOs;
+using System.Runtime;
+
+namespace MagicVilla.VillaWeb;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+        CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+        CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+    }
+}
---
+++ MagicVilla.VillaWeb/Services/BaseService.cs
@@ -0,0 +1,72 @@
+﻿using MagicVilla.VillaWeb.Models;
+using MagicVilla.VillaWeb.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla.VillaWeb.Services;
+
+public class BaseService : IBaseService
+{
+    private readonly IHttpClientFactory httpClient;
+
+    public BaseService(IHttpClientFactory httpClient)
+    {
+        this.responseModel = new();
+        this.httpClient = httpClient;
+    }
+    public APIResponse responseModel { get; set; }
+
+    public async Task<T> SendAsync<T>(APIRequest apiRequest)
+    {
+        try
+        {
+            var client = httpClient.CreateClient("VillaAPI");
+            HttpRequestMessage message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(apiRequest.Url);
+            if (apiRequest.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                    Encoding.UTF8, "application/json");
+            }
+            switch (apiRequest.ApiType)
+            {
+                case Utility.SD.ApiType.GET:
+                    message.Method = HttpMethod.Get;
+                    break;
+                case Utility.SD.ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case Utility.SD.ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case Utility.SD.ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                case Utility.SD.ApiType.PATCH:
+                    message.Method = HttpMethod.Patch;
+                    break;
+            }
+
+            HttpResponseMessage apiResponse = null;
+            
+            apiResponse = await client.SendAsync(message);
+
+            var apiContent = await apiResponse.Content.ReadAsStringAsync();
+            var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+
+            return APIResponse;
+        }
+        catch (Exception ex)
+        {
+            var dto = new APIResponse
+            {
+                ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                IsSuccess = false,
+            };
+            var res = JsonConvert.SerializeObject(dto);
+            var APIResponse = JsonConvert.DeserializeObject<T>(res);
+            return APIResponse;
+        }
+    }
+}
