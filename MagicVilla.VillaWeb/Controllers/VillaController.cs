1cd60bd2de4b1be368798dffad433ddb4b76de0b
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-01 19:49:41 +0330

    update villa (get/post) was developed.

--- MagicVilla.VillaWeb/Controllers/VillaController.cs
+++ MagicVilla.VillaWeb/Controllers/VillaController.cs
@@ -53,4 +53,34 @@ public sealed class VillaController : Controller
 
         return View(model);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateVilla(int villaId)
+    {
+        var response = await _villaService.GetAsync<APIResponse>(villaId);
+        if (response != null && response.IsSuccess)
+        {
+            VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+            return View(_mapper.Map<VillaUpdateDTO>(model));
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+    {
+
+        if (ModelState.IsValid)
+        {
+            var response = await _villaService.UpdateAsync<APIResponse>(model);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVilla));
+            }
+        }
+
+        return View(model);
+    }
 }
