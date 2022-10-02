df6ab3a365bf2cd771d30b9ce01731c54b664d4c
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-02 19:13:33 +0330

    VillaNumberController was developed, and also IncludeProperty was added in the repository.

--- MagicVilla.VillaAPI/Models/DTOs/VillaNumberDTO.cs
+++ MagicVilla.VillaAPI/Models/DTOs/VillaNumberDTO.cs
@@ -7,4 +7,5 @@ public class VillaNumberDTO
     [Required]
     public int VillaNo { get; set; }
     public string SpecialDetails { get; set; }
+    public VillaDTO Villa { get; set; }
 }
--- MagicVilla.VillaAPI/Repository/IRepository/IRepository.cs
+++ MagicVilla.VillaAPI/Repository/IRepository/IRepository.cs
@@ -6,8 +6,8 @@ namespace MagicVilla.VillaAPI.Repository.IRepository;
 public interface IRepository<T> where T : class
 {
     //GetAll : this method can get any LINQ Expression as a filter
-    Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
-    Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+    Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null,string? includeProperties = null);
+    Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true, string? includeProperties = null);
     Task CreateAsync(T entity);
     Task RemoveAsync(T entity);
     Task SaveAsync();
