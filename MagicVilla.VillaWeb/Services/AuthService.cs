5f4445ac772beb88e80b3c005502f36b1a2143a5
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-13 15:58:31 +0330

    add AuthService and IAuthService interface and register it in the DI Container.

---
+++ MagicVilla.VillaWeb/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla.Utility;
+using MagicVilla.VillaWeb.Models.DTOs;
+using MagicVilla.VillaWeb.Services.IServices;
+
+namespace MagicVilla.VillaWeb.Services;
+
+public class AuthService : BaseService, IAuthService
+{
+    private readonly IHttpClientFactory _clientFactory;
+    private string villaUrl;
+
+    public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+    {
+        _clientFactory = clientFactory;
+        villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+    }
+
+    public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+    {
+        return SendAsync<T>(new Models.APIRequest()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = obj,
+            Url = villaUrl + "/api/UsersAuth/Login"
+        });
+    }
+
+    public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+    {
+        return SendAsync<T>(new Models.APIRequest()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = obj,
+            Url = villaUrl + "/api/UsersAuth/Register"
+        });
+    }
+}
+
