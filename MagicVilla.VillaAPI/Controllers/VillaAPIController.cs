050d7f00d9aa21353ea88915271c68619df99789
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-13 11:33:53 +0330

    adding Status Code 401-Unauthorized to DeleteVilla to make it documented!

--- MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla.VillaAPI/Controllers/VillaAPIController.cs
@@ -145,6 +145,7 @@ public class VillaAPIController : ControllerBase
     [Authorize(Roles ="CUSTOM")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<APIResponse>> DeleteVilla(int id)
